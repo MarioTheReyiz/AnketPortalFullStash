7546496c488a1c995b0992e7acd2dd590da1d0ad
MarioTheReyiz <msdn7788@gmail.com>
2026-05-02 22:08:17 +0300

    refresh token api bağlantısı

--- AnketPortal.UI/wwwroot/js/site.js
+++ AnketPortal.UI/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Tüm isteklere token ekle
+$.ajaxSetup({
+    beforeSend: function (xhr) {
+        var token = localStorage.getItem("token");
+        if (token) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        }
+    }
+});
 
-// Write your JavaScript code.
+// Oturum biterse (401) otomatik yenileme yap
+$(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
+    if (jqXHR.status === 401) {
+        var refreshToken = localStorage.getItem("refreshToken");
+        if (refreshToken) {
+            $.ajax({
+                url: "https://localhost:7094/api/Auth/RefreshToken", // Kendi portunla aynı olduğundan emin ol
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(refreshToken),
+                async: false,
+                success: function (res) {
+                    localStorage.setItem("token", res.data.accessToken);
+                    localStorage.setItem("refreshToken", res.data.refreshToken);
+                    location.reload(); // Yeni tokenla sayfayı tazele
+                },
+                error: function () {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("refreshToken");
+                    window.location.href = "/Auth/Login";
+                }
+            });
+        } else {
+            window.location.href = "/Auth/Login";
+        }
+    }
+});
